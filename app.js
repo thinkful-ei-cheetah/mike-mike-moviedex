277342ef52fe113173d9a6fd5ca894f1eab53cba
unknown <kpnba90@gmail.com>
2019-04-30 15:05:28 -0400

    first commit

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const cors = require('cors')
 const helmet = require('helmet')
 
 const app = express()
+
+app.use(morgan('common'))
+
+app.use((req, res) => {
+  res.send('Hello, world!')
+})
+
+app.listen(3000, () => {
+  console.log('running!')
+})
